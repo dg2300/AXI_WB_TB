f795daa81fcbecafafff6955a336bda0c6fbb00b
dg2300 <125730602+dg2300@users.noreply.github.com>
2023-02-20 15:37:33 +0530

    Update run_this.sh

--- cfg/run_this.sh
+++ cfg/run_this.sh
@@ -2,8 +2,8 @@
 #COMPILE COMMAND
 #============
 
-#vcs  -timescale=1ns/1ns -licqueue -kdb -debug_access+all -lca -ntb_opts uvm -sverilog +define+ENABLE_DUMP  +incdir+ $UVM_HOME/src -f $WBTB/cfg/filelist.f  &&
-vcs -timescale=1ns/1ns -licqueue -kdb -debug_access+all -lca -ntb_opts uvm -sverilog +incdir+ $UVM_HOME/src  -f $WBTB/cfg/filelist.f   &&
+vcs  -timescale=1ns/1ns -licqueue -kdb -debug_access+all -lca -ntb_opts uvm -sverilog +define+ENABLE_DUMP  +incdir+ $UVM_HOME/src -f $WBTB/cfg/filelist.f  &&
+#vcs -timescale=1ns/1ns -licqueue -kdb -debug_access+all -lca -ntb_opts uvm -sverilog +incdir+ $UVM_HOME/src  -f $WBTB/cfg/filelist.f   &&
 
 echo '########################################################################################'
 echo '#################################BUILD DONE#############################################'
