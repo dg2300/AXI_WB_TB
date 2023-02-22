7dbaf77a95622312ec92712aca320e13c064e006
dg2300 <125730602+dg2300@users.noreply.github.com>
2023-02-22 20:30:22 +0530

    02_22_23 8:30pm update

--- cfg/run_this.sh
+++ cfg/run_this.sh
@@ -1,18 +1,74 @@
+#!/bin/bash
 #============
 #COMPILE COMMAND
 #============
+echo "Running run_this.sh..."
+echo "#################################STARTING SCRIPT##########################################"
+if [[ $1 == "w_dump" ]]; then
+  echo "Starting Compile with dump..."
+  echo "##########################################################################################"
+  vcs  -timescale=1ns/1ns -licqueue -kdb -debug_access+all -lca -ntb_opts uvm -sverilog +define+ENABLE_DUMP  +incdir+ $UVM_HOME/src -f $WBTB/cfg/filelist.f -l target.log  &
+    PID=$!
+    wait $PID
+else
+  echo "Starting Compile without dump..."
+  echo "##########################################################################################"
+  vcs  -timescale=1ns/1ns -licqueue -kdb -debug_access+all -lca -ntb_opts uvm -sverilog +incdir+ $UVM_HOME/src -f $WBTB/cfg/filelist.f -l target.log &
+    PID=$!
+    wait $PID
+fi
 
-vcs  -timescale=1ns/1ns -licqueue -kdb -debug_access+all -lca -ntb_opts uvm -sverilog +define+ENABLE_DUMP  +incdir+ $UVM_HOME/src -f $WBTB/cfg/filelist.f  &&
+ if [ $? -eq 0 ]
+ then
+   echo "########################################################################################"
+   echo "#################################BUILD SUCCESS##########################################"
+   echo "########################################################################################"
+ else
+   echo "!!!!!!!!!!!!!!!!!!!!!!!!!!BUILD FAILED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+   exit 1
+ fi
+
+
+#vcs  -timescale=1ns/1ns -licqueue -kdb -debug_access+all -lca -ntb_opts uvm -sverilog +define+ENABLE_DUMP  +incdir+ $UVM_HOME/src -f $WBTB/cfg/filelist.f  &&
 #vcs -timescale=1ns/1ns -licqueue -kdb -debug_access+all -lca -ntb_opts uvm -sverilog +incdir+ $UVM_HOME/src  -f $WBTB/cfg/filelist.f   &&
 
-echo '########################################################################################'
-echo '#################################BUILD DONE#############################################'
-echo '########################################################################################'
 #============
 #RUN COMMAND
 #============
-simv +UVM_TESTNAME=axi_test +UVM_TIMEOUT=150ns -l run.log && 
+if [ -f "simv" ]; then
+    echo '#################################EXECUTION START#########################################'
+
+    echo "simv exists. Running command..."
+    # command to run if simv exists
+    simv +UVM_TESTNAME=axi_test +UVM_TIMEOUT=150ns +AXI_WRITE_REQ -l run.log &
+    PID=$!
+    wait $PID
+    echo '########################################################################################'
+    echo '#################################EXEC DONE##############################################'
+    echo '########################################################################################'
+else
+    echo "simv does not exist."
+    echo '!!!!!!!!!!!!!!!!!!!!!!!!!!BUILD FAILED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+    exit 1
+fi
+ 
 #============
 #DUMP COMMAND
 #============
-verdi -ssf novas.fsdb
+if [[ $1 == "w_dump" ]]; then
+  echo "Starting dump..."
+  verdi -ssf novas.fsdb &
+echo '########################################################################################'
+echo '#################################DUMP DONE##############################################'
+echo '########################################################################################'
+else
+  echo "No dump... Exiting script"
+fi
+
+echo '########################################################################################'
+echo '#################################END OF SCRIPT##########################################'
+echo '########################################################################################'
+
+
+exit 0
+
--- verif/test/new_test/test_1.sv
+++ verif/test/new_test/test_1.sv
@@ -18,35 +18,59 @@ class axi_test extends uvm_test ;
 
 
  	 //build phase  
-    function void build_phase(uvm_phase phase);  
-        super.build_phase(phase); 
-        env_h = env::type_id::create("env_h",this);
-        axi_read_sequence_h = axi_read_sequence::type_id::create("axi_read_sequence_h",this);
-        axi_random_sequence_h = axi_random_sequence::type_id::create("axi_random_sequence_h",this);
-        axi_write_sequence_h = axi_write_sequence::type_id::create("axi_read_sequence_h",this);
-    endfunction 
+ 	 function void build_phase(uvm_phase phase);  
+    super.build_phase(phase); 
+    env_h = env::type_id::create("env_h",this);
+    axi_read_sequence_h = axi_read_sequence::type_id::create("axi_read_sequence_h",this);
+    axi_random_sequence_h = axi_random_sequence::type_id::create("axi_random_sequence_h",this);
+    axi_write_sequence_h = axi_write_sequence::type_id::create("axi_read_sequence_h",this);
+   endfunction 
+
+  //---------------------------------------
+  // end_of_elobaration phase
+  //---------------------------------------  
+  virtual function void end_of_elaboration();
+    //print's the topology
+    print();
+  endfunction
 
    task run_phase(uvm_phase phase);
     
     phase.raise_objection(this);
 
-      if($test$plusargs("AXI_READ_REQ"))begin
-        `uvm_info(get_type_name(),$sformatf("DEBUG Sending AXI read req sequence"),UVM_LOW);
-        axi_read_sequence_h.start(env_h.axi_agnt.axi_seqr);
-      end 
       if($test$plusargs("AXI_WRITE_REQ"))begin
         `uvm_info(get_type_name(),$sformatf("DEBUG Sending AXI write req sequence"),UVM_LOW);
         axi_write_sequence_h.start(env_h.axi_agnt.axi_seqr);
-      end else begin
+      end 
+      else if($test$plusargs("AXI_READ_REQ"))begin
         `uvm_info(get_type_name(),$sformatf("DEBUG Sending AXI read req sequence"),UVM_LOW);
+        axi_read_sequence_h.start(env_h.axi_agnt.axi_seqr);
+      end else begin
+        `uvm_info(get_type_name(),$sformatf("DEBUG Sending AXI random req sequence"),UVM_LOW);
         axi_random_sequence_h.start(env_h.axi_agnt.axi_seqr);
       end
-
+   
       //#1000ns;
     phase.drop_objection(this);
      
-    phase.phase_done.set_drain_time(this,50);
+    //phase.phase_done.set_drain_time(this,50);
   endtask : run_phase
 
+ function void report_phase(uvm_phase phase);
+   uvm_report_server svr;
+   super.report_phase(phase);
+   
+   svr = uvm_report_server::get_server();
+   if(svr.get_severity_count(UVM_FATAL)+svr.get_severity_count(UVM_ERROR)>0) begin
+     `uvm_info(get_type_name(), "---------------------------------------", UVM_NONE)
+     `uvm_info(get_type_name(), "----            TEST FAIL          ----", UVM_NONE)
+     `uvm_info(get_type_name(), "---------------------------------------", UVM_NONE)
+    end
+    else begin
+     `uvm_info(get_type_name(), "---------------------------------------", UVM_NONE)
+     `uvm_info(get_type_name(), "----           TEST PASS           ----", UVM_NONE)
+     `uvm_info(get_type_name(), "---------------------------------------", UVM_NONE)
+    end
+  endfunction 
 
 endclass
